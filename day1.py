e6fabd4b1d0e291b8d4c49771bb6fb39a6ac1229
dnell <mail@danienell.com>
2024-01-13 19:29:25 +0000

    test

---
+++ day1.py
@@ -0,0 +1,22 @@
+def plusMinus(arr):
+    # Write your code here
+    positive = 0
+    negative = 0
+    zero = 0
+    denominator = 0
+    
+    for item in arr:
+        denominator += 1
+        if item == 0:
+            zero += 1
+        elif item < 0:
+            negative += 1
+        elif item > 0:
+            positive +=1
+    
+    f_positive = float(positive / denominator)
+    f_negative = float(negative / denomitator)
+    f_zero = float(zero / denominator)
+    print(f_positive)
+    print(f_negative)
+    print(f_zero)
