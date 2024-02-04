55704c4d61afc35461d856d84f55c9bac651fc1a
iniTwakkie <dnell.personal@gmail.com>
2024-02-04 20:22:32 +0000

    suck it

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-# lets see
+# lets have a go at this
+
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+r = requests.get(url)
+
+data = r.json()
+
+print(data)
+
+
+
